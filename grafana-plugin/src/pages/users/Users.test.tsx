f78a8554680be73c842aae80c62829ee9e79dfc7
Rares Mardare <rares.mardare@grafana.com>
2023-03-22 10:37:22 +0200

    updated test

--- grafana-plugin/src/pages/users/Users.test.tsx
+++ grafana-plugin/src/pages/users/Users.test.tsx
@@ -1,56 +1,30 @@
 import 'jest/matchMedia.ts';
-import { createMemoryHistory } from 'history';
 import React from 'react';
-
+import { createMemoryHistory } from 'history';
 import { fireEvent, render, screen } from '@testing-library/react';
-
 import { Users } from './Users';
 import { Route, Router } from 'react-router-dom';
 
 import * as useStoreHelper from 'state/useStore';
-
-jest.mock('state/useStore', () => ({ useStore: jest.fn() }));
+import * as authorizationHelper from 'utils/authorization';
 
 import { RootBaseStore } from 'state/rootBaseStore';
 import { MockedUserStore } from './__mocks__/MockedUserStore';
 import { users } from './__mocks__/users';
 import { team } from './__mocks__/teams';
 import { MockedTeamStore } from './__mocks__/MockedTeamStore';
-jest.mock('models/base_store');
+import { act } from 'react-test-renderer';
 
-jest.mock('utils/authorization', () => ({
-  ...jest.requireActual('utils/authorization'),
-  isUserActionAllowed: jest.fn().mockReturnValue(true),
-}));
-
-jest.mock('@grafana/runtime', () => ({
-  config: {
-    featureToggles: {
-      topNav: false,
-    },
-  },
-
-  locationService: {
-    partial: jest.fn(),
-    replace: jest.fn(),
-    push: jest.fn(),
-  },
-  getBackendSrv: () => ({
-    get: jest.fn(),
-    post: jest.fn(),
-  }),
-}));
-
-const queryMock = {
-  p: 1,
-};
-
-let rootStore;
-
+const queryMock = { p: 1 };
 const userPaths = ['/a/grafana-oncall-app/users', '/a/grafana-oncall-app/users/:id'];
 
 describe('Users', () => {
+  let rootStore: any;
+
   beforeEach(() => {
+    jest.resetModules();
+    jest.spyOn(authorizationHelper, 'isUserActionAllowed').mockReturnValue(true);
+
     initStore();
   });
 
@@ -67,7 +41,7 @@ describe('Users', () => {
     };
   }
 
-  test('My Attempt', () => {
+  test('Clicking View My Profile button opens User Settings', () => {
     const history = createMemoryHistory({
       initialEntries: [userPaths[0]],
     });
@@ -83,7 +57,33 @@ describe('Users', () => {
       </Router>
     );
 
-    const viewMyProfileButton = screen.getByTestId<HTMLButtonElement>('view-my-profile');
-    fireEvent.click(viewMyProfileButton);
+    act(() => {
+      const viewMyProfileButton = screen.getByTestId<HTMLButtonElement>('view-my-profile');
+      fireEvent.click(viewMyProfileButton);
+
+      const userSettings = screen.getByTestId<HTMLDivElement>('user-settings');
+      expect(userSettings).toBeDefined();
+    });
   });
 });
+
+/*
+ * MOCKED MODULES
+ */
+
+jest.mock('@grafana/runtime', () => ({
+  config: {
+    featureToggles: {
+      topNav: false,
+    },
+  },
+  locationService: {
+    partial: jest.fn(),
+    replace: jest.fn(),
+    push: jest.fn(),
+  },
+  getBackendSrv: () => ({
+    get: jest.fn(),
+    post: jest.fn(),
+  }),
+}));
